9fdc616594f812ab566b37a23eef2032f6445b13
liceist2008@mail.ru <liceist2008@mail.ru>
2019-11-13 16:28:45 +0600

    upd private chat

--- webapplication/ChatHub.cs
+++ webapplication/ChatHub.cs
@@ -12,19 +12,12 @@ namespace webapplication
 		[Authorize]
 		public async Task SendToAll(string name, string message, string to)
 		{
-			//Clients.All.SendAsync("sendToAll", name, message);
-
 			var userName = Context.User.Identity.Name;
 
 			if (Context.UserIdentifier != to) // если получатель и текущий пользователь не совпадают
 				await Clients.User(Context.UserIdentifier).SendAsync("Receive", name, message, userName);
 			await Clients.User(to).SendAsync("Receive", name, message, userName);
 		}
-
-		public override async Task OnConnectedAsync()
-		{
-			await Clients.All.SendAsync("Notify", $"Приветствуем {Context.UserIdentifier}");
-			await base.OnConnectedAsync();
-		}
+		
 	}
 }
