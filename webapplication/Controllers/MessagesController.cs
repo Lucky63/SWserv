819df45a38f94696f87a8ebff680a3e8ddd58e58
liceist2008@mail.ru <liceist2008@mail.ru>
2019-12-05 22:18:25 +0600

    delete tape component and delete tapes metgods

--- webapplication/Controllers/MessagesController.cs
+++ webapplication/Controllers/MessagesController.cs
@@ -34,53 +34,6 @@ namespace webapplication.Controllers
 		public async Task <List<string>> GetMessagesAsync(int id, int FriendId)
 		{
 			return await _messageService.GetMessagesAsync(id, FriendId);			
-		}
-
-		
-		[HttpPost("[action]/{id}"), Route("SaveMessageForTapeAsync")]
-		[HttpPost("[action]/{id}/{message}"), Route("SaveMessageForTapeAsync")]
-		public async Task SaveMessageForTapeAsync(int id, string message)
-		{
-			var currentUser = await db.Users.Include(x=>x.UserFriends).ThenInclude(x=>x.Friend).FirstOrDefaultAsync(x=>x.Id == id);
-			if(currentUser != null)
-			{
-				foreach (var i in currentUser.UserFriends)
-				{
-					db.Tapes.Add(new Tape { UserId = currentUser.Id, FriendId = i.FriendId, Message = message });
-					await db.SaveChangesAsync();
-				}
-			}
-
-		}
-
-		[HttpGet("[action]/{id}"), Route("GetMessagesFromTapes")]		
-		public async Task<List<string>> GetMessagesFromTapes(int id)
-		{
-			List<string> mesages = new List<string>();
-			User User = await db.Users.FirstOrDefaultAsync(x => x.Id == id);
-			string username = User.UserName;			
-
-			var usermessages = db.Tapes
-			  .Where(x => ( x.FriendId == id)||
-						  (x.UserId ==  id)).ToList();
-
-			foreach (var i in usermessages)
-			{
-				if (i.UserId == id)
-				{
-					var sentUser = db.Users.FirstOrDefault(x => x.Id == id);
-					mesages.Add($"{sentUser.UserName}- {i.Message}");
-				}
-				if (i.FriendId == id)
-				{
-					var sentUser = db.Users.FirstOrDefault(x => x.Id == i.UserId);
-					mesages.Add($"{sentUser.UserName}- {i.Message}");
-				}
-				
-			}
-
-			return mesages;
-
-		}
+		}		
 	}
 }
