219800e790212b5c15b429c4d43e64655c30ee94
liceist2008@mail.ru <liceist2008@mail.ru>
2019-12-08 14:06:38 +0600

    added likecounter for PhotoViewModel

--- webapplication/Models/PhotosViewModel.cs
+++ webapplication/Models/PhotosViewModel.cs
@@ -11,8 +11,10 @@ namespace webapplication.Models
 		{
 			id = photos.Id;
 			path = photos.PhotoPath;
+			likeCounter = photos.LikeCounter;
 		}
 		public int id { get; set; }
 		public string path { get; set; }
+		public int likeCounter { get; set; }
 	}
 }
